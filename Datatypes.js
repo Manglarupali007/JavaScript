0a79067b1d797ab4ff52076401369553001f981a
Rupali Mangla <rupalimangla.9842@gmail.com>
2025-07-20 19:04:07 +0530

    Create Datatypes.js

---
+++ Datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all JS code as new version
+
+//pop-up
+// alert("Hello"); used in browser only
+
+
+//arithmetic operation
+console.log(3+3); 
+
+
+//Datatypes
+//1. Numbers - range 2 to power 53
+//2. bigint - for large numbers
+//3. string - used in single and double quotes
+//4. boolean - true or false
+//5. null - standalone value / empty value - can be assigned as null
+//6. undefined - variable declared but value not assigned
+//7. symbol - unique
+
+
+console.log(typeof(123)); //print number
+console.log(typeof(false)); //boolean
+
+
+//object
+console.log(typeof(null)); //return object
+
+
+//undefined
+console.log(typeof(undefined));//print undefined
+
+
